307130def0ae0806f13b540e16cc0891b23fa1e3
MariaPaula <mariapaulaembuspolanco@gmail.com>
2023-08-29 09:14:40 -0500

    actualizacion06

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import { pool } from './db.js'
+import employeesRoutes from './routes/employees.routes.js'
+
 
 const app = express()
 
@@ -7,11 +9,6 @@ app.get('/ping',async (req,res) => {
     const result = await pool.query('SELECT 1 + 1 AS result')
     res.json(result)
 })
-
-app.get('/employees',(req,res)=>res.send('Obteniendo Empleados'))
-app.post('/employees',(req,res)=>res.send('Creando Empleados'))
-app.put('/employees',(req,res)=>res.send('Actualizando Empleados'))
-app.delete('/employees',(req,res)=>res.send('Eliminando Empleados'))
 
 app.listen(5000)
 console.log('Servidor en el puerto', 5000)
