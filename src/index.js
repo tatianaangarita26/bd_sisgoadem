eb1b7ca2ad1fe612ae13ae943167518ab2d31aee
MariaPaula <mariapaulaembuspolanco@gmail.com>
2023-08-29 10:37:09 -0500

    actualizacion08

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import express from 'express'
 import { pool } from './db.js'
-import employeesRoutes from './routes/employees.routes.js'
+import employeesRoutes from './routes/sisgoadem.routes.js'
 
 
 const app = express()
 
 app.get('/ping',async (req,res) => {
-    const [result] = await pool.query('SELECT 1 + 1 AS result')
+    const [result] = await pool.query('SELECT "Pong" AS result')
     res.json(result[0])
 })
 
-app.use(employeesRoutes)
+app.use(sisgoademRoutes)
 
 app.listen(5000)
 console.log('Servidor en el puerto', 5000)
